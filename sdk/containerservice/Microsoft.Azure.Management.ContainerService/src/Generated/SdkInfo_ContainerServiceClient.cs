337f5be7cb3574d6744103e9a1af1e0b8474aca7
SDK Automation <sdkautomation@microsoft.com>
2020-07-07 03:28:07 +0000

    Generated from ccf7973078b9b1ffb7d25fca6849c034b2ae9cdc
    Add licenseType in WindowsProfile for AKS

--- sdk/containerservice/Microsoft.Azure.Management.ContainerService/src/Generated/SdkInfo_ContainerServiceClient.cs
+++ sdk/containerservice/Microsoft.Azure.Management.ContainerService/src/Generated/SdkInfo_ContainerServiceClient.cs
@@ -19,13 +19,13 @@ namespace Microsoft.Azure.Management.ContainerService
           {
               return new Tuple<string, string, string>[]
               {
-                new Tuple<string, string, string>("ContainerService", "AgentPools", "2020-06-01"),
+                new Tuple<string, string, string>("ContainerService", "AgentPools", "2020-07-01"),
                 new Tuple<string, string, string>("ContainerService", "ContainerServices", "2017-07-01"),
                 new Tuple<string, string, string>("ContainerService", "ContainerServices", "2019-08-01"),
-                new Tuple<string, string, string>("ContainerService", "ManagedClusters", "2020-06-01"),
+                new Tuple<string, string, string>("ContainerService", "ManagedClusters", "2020-07-01"),
                 new Tuple<string, string, string>("ContainerService", "OpenShiftManagedClusters", "2019-04-30"),
-                new Tuple<string, string, string>("ContainerService", "Operations", "2020-06-01"),
-                new Tuple<string, string, string>("ContainerService", "PrivateEndpointConnections", "2020-06-01"),
+                new Tuple<string, string, string>("ContainerService", "Operations", "2020-07-01"),
+                new Tuple<string, string, string>("ContainerService", "PrivateEndpointConnections", "2020-07-01"),
               }.AsEnumerable();
           }
       }
