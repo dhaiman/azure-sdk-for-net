dd2746c806bbe5a5f509cbbfbfc36b526d65f65a
SDK Automation <sdkautomation@microsoft.com>
2020-08-06 04:14:02 +0000

    Generated from c38ba8eefb9c89653277d7230f5b411f9439f6a1
    +gatewayhostnameconfiguration protocol changes

--- sdk/apimanagement/Microsoft.Azure.Management.ApiManagement/src/Generated/Models/ProductTagResourceContractProperties.cs
+++ sdk/apimanagement/Microsoft.Azure.Management.ApiManagement/src/Generated/Models/ProductTagResourceContractProperties.cs
@@ -53,12 +53,12 @@ namespace Microsoft.Azure.Management.ApiManagement.Models
         /// immediately after subscribing. If true, administrators must
         /// manually approve the subscription before the developer can any of
         /// the product’s APIs. Can be present only if subscriptionRequired
-        /// property is present and has a value of true.</param>
+        /// property is present and has a value of false.</param>
         /// <param name="subscriptionsLimit">Whether the number of
         /// subscriptions a user can have to this product at the same time. Set
         /// to null or omit to allow unlimited per user subscriptions. Can be
         /// present only if subscriptionRequired property is present and has a
-        /// value of true.</param>
+        /// value of false.</param>
         /// <param name="state">whether product is published or not. Published
         /// products are discoverable by users of developer portal. Non
         /// published products are visible only to administrators. Default
