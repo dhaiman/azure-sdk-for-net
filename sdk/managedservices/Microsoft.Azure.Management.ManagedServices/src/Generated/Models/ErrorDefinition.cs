40f587722847059a9b0398004bdfdae8e82ccdc8
SDK Automation <sdkautomation@microsoft.com>
2020-10-31 00:21:35 +0000

    Generated from 8277f20ca4f2779b5a35a0d64ac9d6af2df757fc

--- sdk/managedservices/Microsoft.Azure.Management.ManagedServices/src/Generated/Models/ErrorDefinition.cs
+++ sdk/managedservices/Microsoft.Azure.Management.ManagedServices/src/Generated/Models/ErrorDefinition.cs
@@ -12,32 +12,36 @@ namespace Microsoft.Azure.Management.ManagedServices.Models
 {
     using Microsoft.Rest;
     using Newtonsoft.Json;
+    using System.Collections;
+    using System.Collections.Generic;
     using System.Linq;
 
     /// <summary>
     /// Error response indicates Azure Resource Manager is not able to process
     /// the incoming request. The reason is provided in the error message.
     /// </summary>
-    public partial class ErrorResponseError
+    public partial class ErrorDefinition
     {
         /// <summary>
-        /// Initializes a new instance of the ErrorResponseError class.
+        /// Initializes a new instance of the ErrorDefinition class.
         /// </summary>
-        public ErrorResponseError()
+        public ErrorDefinition()
         {
             CustomInit();
         }
 
         /// <summary>
-        /// Initializes a new instance of the ErrorResponseError class.
+        /// Initializes a new instance of the ErrorDefinition class.
         /// </summary>
         /// <param name="code">Error code.</param>
         /// <param name="message">Error message indicating why the operation
         /// failed.</param>
-        public ErrorResponseError(string code, string message)
+        /// <param name="details">Internal error details.</param>
+        public ErrorDefinition(string code, string message, IList<ErrorDefinition> details = default(IList<ErrorDefinition>))
         {
             Code = code;
             Message = message;
+            Details = details;
             CustomInit();
         }
 
@@ -59,6 +63,12 @@ namespace Microsoft.Azure.Management.ManagedServices.Models
         public string Message { get; set; }
 
         /// <summary>
+        /// Gets or sets internal error details.
+        /// </summary>
+        [JsonProperty(PropertyName = "details")]
+        public IList<ErrorDefinition> Details { get; set; }
+
+        /// <summary>
         /// Validate the object.
         /// </summary>
         /// <exception cref="ValidationException">
@@ -73,6 +83,16 @@ namespace Microsoft.Azure.Management.ManagedServices.Models
             if (Message == null)
             {
                 throw new ValidationException(ValidationRules.CannotBeNull, "Message");
+            }
+            if (Details != null)
+            {
+                foreach (var element in Details)
+                {
+                    if (element != null)
+                    {
+                        element.Validate();
+                    }
+                }
             }
         }
     }
